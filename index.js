9b5dad14bd53dcdfe40d32180283fcf4aadbe887
hamid <ahmidatabdelhamid@gmail.com>
2023-07-26 18:16:01 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1 = 31;
+const num2 = 2;
+const num3 = 16;
+const num4 = 6;
 
+let multiply = num1 * num2;
+//==> 62
+
+
+let random = Math.ceil(Math.random() * 10) + 1;
+
+
+let mod = num3 % num4;
+
+
+let max = Math.max(0, 2, 5, 8, 20, 6, 3, 19);
 // Write your code here
